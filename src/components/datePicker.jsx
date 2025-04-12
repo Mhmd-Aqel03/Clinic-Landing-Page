6cab46c458844ac4c99784f841c143bff0df400a
Mohammed <mhmdaqelbs@gmail.com>
2025-04-12 09:39:56 +0300

    added date state

--- src/components/datePicker.jsx
+++ src/components/datePicker.jsx
@@ -2,29 +2,23 @@ import * as React from "react";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
-import dayjs from "dayjs"
+import dayjs from "dayjs";
 
-const datePicker = () => {
-  const [value, setValue] = React.useState(dayjs());
-
+const datePicker = ({ value, onChange }) => {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <StaticDatePicker
         label="Pick a date"
         value={value}
-        onChange={(newValue) => setValue(newValue)}
-
+        onChange={onChange}
         // Removes the "CANCEL" and "OK"
         slotProps={{
-          actionBar: { actions: [] }
+          actionBar: { actions: [] },
         }}
-
         // Removes the text above the calender
-        slots={
-          {
-            toolbar:null
-          }
-        }
+        slots={{
+          toolbar: null,
+        }}
       />
     </LocalizationProvider>
   );
--- src/pages/bookPage.jsx
+++ src/pages/bookPage.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import DatePicker from '../components/datePicker'
+import React, { useEffect, useState } from "react";
+import DatePicker from "../components/datePicker";
+import dayjs from "dayjs";
 const BookPage = () => {
+  const [date, setDate] = useState(dayjs());
+  useEffect(() => {
+    
+  })
   return (
-    <div className="flex flex-col mt-[75px] mx-4">
-      <div className="text-2xl font-poppins">
-        Please pick a date and time.
-      </div>
+    <div className="flex flex-col mt-[75px] mx-4 items-center">
+      <div className="text-2xl font-poppins">Please pick a date and time.</div>
       <div className="">
-        <DatePicker />
+        <DatePicker
+          value={date}
+          onChange={(newDate) => {
+            setDate(newDate);
+          }}
+        />
       </div>
+      <div>{date.format("YYYY/MM/DD")}</div>
     </div>
   );
-}
+};
 
-export default BookPage
+export default BookPage;
